5086b790995bf558e9d568523caea82f54c99588
smule <smule@ncsu.edu>
2024-10-31 18:41:06 -0400

    ui changes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -25,10 +25,15 @@ const LandingPage = () => {
             <div className="col-md-8 text-center">
               <h1 className="main-title">🎬 PopcornPicks 🍿</h1>
               <h2 className="subtitle">Your Personal Movie Recommender</h2>
+              
+
               <p className="description">
-                Discover personalized movie recommendations by selecting up to 5 of your favorite films.
-                Create a watchlist and have it conveniently sent to your email.
-                Enjoy movies at your own pace, on your terms.
+               
+
+              Discover personalized movie recommendations by selecting up to 5 of your favorite films. 
+                Create your own watchlist and access IMDb ratings and links for informed choices. 
+                With PopcornPicks, enjoy movies at your own pace, on your terms. 
+                Let's make every movie night a blockbuster!
               </p>
               <button 
                 id="getStartedButton" 
